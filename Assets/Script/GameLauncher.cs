f208a972933f1f24d8e3dd2f1a268ea3b0f76350
MakinoYuto <it242241@trident.ac.jp>
2026-07-07 15:25:30 +0900

    ネットワーク同期テスト用2026/07/07

--- Assets/Script/GameLauncher.cs
+++ Assets/Script/GameLauncher.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class GameLauncher : MonoBehaviour
 {
+    public GameObject playerPrefab; // プレイヤーのPrefabをInspectorで設定 
     private async void Start()
     {
+
         // NetworkRunnerを生成してゲーム（部屋）を開始
         var runner = gameObject.AddComponent<NetworkRunner>();
+        runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, runner.LocalPlayer);
 
         await runner.StartGame(new StartGameArgs
         {
